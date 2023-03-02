def58e5a4c1248978647378a0bc912c73ba6ad06
AyomideCode <thebeep41@gmail.com>
2023-03-02 15:46:27 -0500

    No Javadocs yet; every method works but intersect() and difference()

--- lsp_spring_2023/src/org/howard/edu/lsp/assignment4/Driver.java
+++ lsp_spring_2023/src/org/howard/edu/lsp/assignment4/Driver.java
@@ -1,6 +1,4 @@
 package org.howard.edu.lsp.assignment4;
-import org.howard.edu.lsp.assignment4.IntegerSet;
-import org.howard.edu.lsp.assignment4.IntegerSetException;
 
 /**
  * @author AyomideCode
@@ -10,4 +8,80 @@ import org.howard.edu.lsp.assignment4.IntegerSetException;
  */
 public class Driver {
 
+	// intersect() might be broken.
+	// difference() is broken.
+	
+	public static void main(String[] args) throws IntegerSetException {
+		System.out.println("This program will go through and execute all the methods in IntegerSet.java. \n");
+		
+		System.out.println("Making a new set, Set 1.");
+		IntegerSet set1 = new IntegerSet();
+		
+		System.out.println("Testing add(). Adding items to Set 1...");
+		set1.add(1);
+		set1.add(2);
+		set1.add(3);
+		System.out.println("Value of Set 1 is: \n" + set1.toString() + "\n");
+		
+		System.out.println("Testing clear().");
+		set1.clear();
+		System.out.println("Set 1, as a result of clearing: \n" + set1.toString());
+		System.out.println("(Since Set 1 was cleared, toString() printed nothing as to represent the now empty set.) \n");
+		
+		System.out.println("Adding those same three items back to Set 1...");
+		set1.add(1);
+		set1.add(2);
+		set1.add(3);
+		System.out.println("Value of Set 1 is back to being: " + set1.toString() + "\n");
+		
+		System.out.println("Testing length(). Length of Set 1 is: \n" + set1.length() + "\n");
+	
+		System.out.println("Making another new set, Set 2. Adding items to it...");
+		
+		IntegerSet set2 = new IntegerSet();
+		set2.add(1);
+		set2.add(2);
+		set2.add(3);
+		set2.add(4);
+		set2.add(5);
+		
+		System.out.println("Value of Set 2 is: \n" + set2.toString() + "\n");
+
+		System.out.println("Testing equals(). Does Set 1 have the same values as Set 2? True/false: \n" + set1.equals(set2) + "\n");
+
+		System.out.println("Testing contains(). Does Set 1 contain the value '2'? True/false: \n" + set1.contains(2) + "\n");
+
+		System.out.println("Testing largest(). Largest item in Set 1: \n" + set1.largest() + "\n");
+		
+		System.out.println("Testing smallest(). Smallest item in Set 1: \n" + set1.smallest() + "\n");
+
+		System.out.println("Adding an extra integer 999 to Set 1, that will soon be removed...");
+		set1.add(999);
+		System.out.println("New value of Set 1 is: " + set1.toString() + "\n");
+
+		System.out.println("Testing remove(). Removing 999 from Set 1...");
+		set1.remove(999);
+		System.out.println("Value of Set 1 now is: " + set1.toString() + "\n");
+
+		System.out.println("Testing union().");
+		System.out.println("Value of Set 1 is: " + set1.toString());
+		System.out.println("Value of Set 2 is: " + set2.toString());
+		set1.union(set2);																		// union of set 1 and set 2
+		System.out.println("Result of union of Set 1 and Set 2: \n" + set1.toString() + "\n");
+		
+		System.out.println("Testing intersect(). Intersecting Set 1 & Set 2:");
+		set1.intersect(set2);
+		
+		System.out.println("\nTesting difference(). Set 2 - Set 1:");
+		set2.diff(set1);
+		
+		System.out.println("\nTesting isEmpty(). Is Set 1 empty? True/false: \n" + set1.isEmpty() + "\n");
+		System.out.println("Using clear() on Set 1, to test isEmpty on it again...");
+		set1.clear();
+		System.out.println("Testing isEmpty() again. Is Set 1 empty? True/false: \n" + set1.isEmpty() + "\n");
+		
+		System.out.println("Testing IntegerSetException, by using smallest() on the emptied Set 1: \n");
+		set1.smallest();
+	}
+	
 }
--- lsp_spring_2023/src/org/howard/edu/lsp/assignment4/IntegerSet.java
+++ lsp_spring_2023/src/org/howard/edu/lsp/assignment4/IntegerSet.java
@@ -6,11 +6,165 @@ import java.util.Collections;
  * @author AyomideCode
  *
  * This class is made up of multiple functions, taking user-defined sets and manipulating them in Driver.java
- * 
- * Implemented set operations: 
- * ADD TO LIST WHEN FINISHED
+ * Implemented set operations: clear(), length(), equals(), contains(), largest(), smallest(), add(), remove(), union(), intersect(), diff(), isEmpty(), toString()
  */
 
 public class IntegerSet {
+	/**
+	 * @param resource qualifies the location of the file
+	 * @throws IntegerSetException
+	 * @return Driver.java uses the methods in this class to return strings, booleans & int for length(); otherwise, void
+	 */
+	
+    //Makes the default, mutable set, and a getter for it	
+	private ArrayList<Integer> set = new ArrayList<Integer>();
+	public ArrayList<Integer> getSet() {
+		return this.set;
+	}
 
+	// Clears the internal representation of the set
+	public void clear(){
+		set.clear();
+		//System.out.println(set);
+	}
+
+	// Returns the length of the set
+	public int length() {
+		//System.out.println(set.size());
+		return set.size();			
+	}
+
+	/*
+	 * Returns true if the 2 sets are equal, false otherwise;
+	 * Two sets are equal if they contain all of the same values in ANY order.
+	*/
+	public boolean equals(IntegerSet setB){
+		boolean equals;
+		ArrayList<Integer> tempArrayList = setB.getSet();							// creates getter for the set.
+		if(set.containsAll(tempArrayList) && tempArrayList.containsAll(set)) {
+			equals = true;
+		}	
+		else {
+			equals = false;
+		}
+		//System.out.println(equals);
+		return equals;
+	}
+
+	// Returns true if the set contains the value, otherwise false
+	public boolean contains(int value){
+		boolean contains;
+		if (set.contains(value)) {
+			contains = true;
+		}
+		else {
+			contains =  false;
+		}
+		//System.out.println(contains);
+		return contains;
+	}    
+
+	// Returns the largest item in the set; Throws a IntegerSetException if the set is empty 
+	public int largest() throws IntegerSetException{
+		int largest = 0;
+		if(set.isEmpty()) {
+			throw new IntegerSetException("IntegerSetException: The provided list is empty; there is no largest integer.");
+		}
+		else {
+			ArrayList<Integer> sortedList = new ArrayList<Integer>(set);
+			Collections.sort(sortedList);
+			//System.out.println(sortedList.get(sortedList.size() -1));
+			largest = sortedList.get(sortedList.size() -1);
+			return largest;
+		}
+	}
+
+	// Returns the smallest item in the set; Throws a IntegerSetException if the set is empty
+	public int smallest() throws IntegerSetException{
+		int smallest = 0;
+		if(set.isEmpty()) {
+			throw new IntegerSetException("IntegerSetException: The provided list is empty; there is no smallest integer.");
+		}
+		else {
+			ArrayList<Integer> sortedList = new ArrayList<Integer>(set);
+			Collections.sort(sortedList);
+			//System.out.println(sortedList.get(0));
+			smallest = sortedList.get(0);
+			return smallest;
+		}
+	}
+	
+	// Adds an item to the set or does nothing if already there
+ 	public void add(int item){
+ 		if(set.contains(item) == false) {
+ 			set.add(item);			 			
+ 		}
+ 		//System.out.println(set);
+ 	} 
+
+	// Removes an item from the set or does nothing if not there
+	public void remove(int item){
+		ArrayList<Integer> tempArrayList = new ArrayList<Integer>();	// creates getter for the set
+		for (int i=0; i < set.size(); i++) {
+			if(set.get(i) != item){
+				tempArrayList.add(set.get(i));
+				//System.out.println(tempArrayList);
+			}
+		}
+		set = tempArrayList;
+	}
+
+	// Set union
+	public void union (IntegerSet setB) {
+		ArrayList<Integer> tempArrayList = setB.getSet();			// creates getter for the set
+	    for (int i=0; i< tempArrayList.size(); i++) {				// iterates through all elements of passed in ArrayList<Integer)
+	       if (!set.contains(tempArrayList.get(i)))  {				// checks if item is not already in set
+	           add(tempArrayList.get(i));							// if so, adds it
+	       }
+	    }	  
+	}	
+	
+	// Set intersection
+	public void intersect(IntegerSet setB){
+		ArrayList<Integer> tempArrayList = setB.getSet();			// creates getter for the set
+		ArrayList<Integer> fillerList = new ArrayList<Integer>();	// creates getter for the common set elements
+	    for (int i=0; i< tempArrayList.size(); i++) {				// iterates through all elements of passed in ArrayList<Integer)
+	       if (set.contains(tempArrayList.get(i)))  {				// checks if item is already in set
+	           fillerList.add(tempArrayList.get(i));				// adds the item, if not there
+	       }
+	    }
+        System.out.println(fillerList);
+
+	} 
+
+	// Set difference, i.e., s1 - s2
+	public void diff(IntegerSet setB) {
+		ArrayList<Integer> tempArrayList = set;						// creates getter for the set
+		ArrayList<Integer> fillerList = new ArrayList<Integer>();	// creates getter for elements only in set A
+	    for (int i=0; i< tempArrayList.size(); i++) {				// iterates through all elements of passed in ArrayList<Integer)
+	       if (!setB.contains(tempArrayList.get(i)))  {				// checks if item is already in set
+	           fillerList.add(tempArrayList.get(i));				// adds the item, if not there
+	       }
+	    }
+	    System.out.println(fillerList);
+	}
+
+	// Returns true if the set is empty, false otherwise
+	public boolean isEmpty() {
+		boolean empty;
+		empty = (set.size() == 0);
+		//System.out.println(empty);
+		return empty;				 
+	}
+	
+	// Returns a String representation of the set
+	public String toString(){
+		String set_string ="";
+		for (int i = 0; i < set.size(); i++) {
+			set_string = set_string.concat(String.valueOf(set.get(i)) + ", ");
+		}
+		//System.out.println(set_string);
+		return set_string;
+	}
+	
 }
