05f2c30fd6e2c8aaa1841c53c5216f674106a237
AyomideCode <thebeep41@gmail.com>
2023-03-16 14:39:55 -0400

    smallest() test case with exception handling works now

--- lsp_spring_2023/src/org/howard/edu/lsp/assignment5/IntegerSetTest.java
+++ lsp_spring_2023/src/org/howard/edu/lsp/assignment5/IntegerSetTest.java
@@ -1,13 +1,12 @@
 package org.howard.edu.lsp.assignment5;
 
-import java.util.ArrayList;
-import org.howard.edu.lsp.assignment5.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.Assert.*;
 
 import org.junit.jupiter.api.DisplayName;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -34,13 +33,13 @@ public class IntegerSetTest {
 		assertEquals(true, set1.contains(0));
 	}
 	
-//	@Test
-//	@DisplayName("Test case for smallest(), purposefully throwing IntegerSetException")
-//	void testSmallest_IntegerSetException() throws IntegerSetException {
-//		set1.clear();
-//		Throwable exception = assertThrows(IntegerSetException.class, () -> set1.smallest() );
-//		assertEquals("IntegerSetException: The provided list is empty; there is no smallest integer.", exception.getMessage() );
-//	}
+	@Test
+	@DisplayName("Test case for smallest(), purposefully throwing IntegerSetException")
+	void testSmallest_IntegerSetException() throws IntegerSetException {
+		set1.clear();
+		Throwable exception = assertThrows(IntegerSetException.class, () -> set1.smallest() );
+		assertEquals("IntegerSetException: The provided list is empty; there is no smallest integer.", exception.getMessage() );
+	}
 	
 
 }
