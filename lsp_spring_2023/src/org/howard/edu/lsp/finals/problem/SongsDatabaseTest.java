0b0072182e74e9b7efe0270d98dffafe0e2b2054
AyomideCode <thebeep41@gmail.com>
2023-04-27 21:15:30 -0400

    JUnit

--- lsp_spring_2023/src/org/howard/edu/lsp/finals/problem/SongsDatabaseTest.java
+++ lsp_spring_2023/src/org/howard/edu/lsp/finals/problem/SongsDatabaseTest.java
@@ -13,11 +13,16 @@ class SongsDatabaseTest {
 		db.addSong("Rap", "Savage");
 		db.addSong("Rap", "Gin and Juice");
 		db.addSong("Jazz", "Always There");
-		db.addSong("EDM", "A");
-		
-		db.getSongs("Rap");
-		db.getGenreOfSong("A");
-		
+		db.addSong("EDM", "USAO");
+	}
+	
+	@Test
+	void testGetSongs() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetGenreOfSong() {
 		fail("Not yet implemented"); // TODO
 	}
 
